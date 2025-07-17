ce77fd53590ad39f184465d01e5d66f212b2d3c0
Chingapo <115269983+Chingapo@users.noreply.github.com>
2025-07-17 12:11:34 +0530

    Add files via upload

---
+++ tic_tac_toe_app/src/main/java/com/techlabs/model/Player.java
@@ -0,0 +1,30 @@
+package com.techlabs.model;
+
+public class Player {
+	private String name;
+	private char symbol;
+	
+	public Player(String name, char symbol) {
+		super();
+		this.name = name;
+		this.symbol = symbol;
+	}
+
+	public Player() {
+		super();
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public char getSymbol() {
+		return symbol;
+	}
+
+	public void play() {
+		
+	}
+	
+	
+}
---
+++ tic_tac_toe_app/src/main/java/com/techlabs/test/DriverCode.java
@@ -0,0 +1,14 @@
+package com.techlabs.test;
+
+import com.techlabs.model.Game;
+
+public class DriverCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Game game = new Game();
+		game.start();
+				
+	}
+
+}
